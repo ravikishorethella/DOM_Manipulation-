0fd40aa5e7442bef7b76b62c9c6f4f6faef53dd1
ravikishorethella <ravikishorethella@gmail.com>
2017-05-16 20:25:59 -0400

    classList

---
+++ script.js
@@ -0,0 +1,23 @@
+//// DOM Manipulation
+//
+//// we are going to cover different ways of 
+//
+////    1. changing an element's style
+////    2. adding/removing classes
+////    3. changing the content of a tag
+////    4. changing attributes(src,href,etc)
+//
+////applying all the styles in html head section and we will see how to call that in js
+//// applying the .big class fro the p tag
+//
+//var p = document.querySelector("p");
+//// if we want to add the classlist to p tag then we can use the following
+//p.classList.add("big");
+//
+////to remove the class list we can use 
+//p.classList.remove("big");
+//
+////toggle is also a method which will be very helpful
+//p.classList.toggle("big");
+
+////=======================================================================================================
